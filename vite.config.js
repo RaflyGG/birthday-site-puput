b4f3ea93b03edf2ee9edd026b863da1dbe867586
RaflyGG <muhammadraflyrbn@gmail.com>
2026-09-19 19:44:30 +0700

    Build

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/birthday-site-puput/', // PENTING: Ganti dengan nama repository GitHub-mu nanti!
   server: {
     open: true,
   },
